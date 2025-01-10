16f499f30214c1f0a2bbde60a75c2992b4d16f1c
Diakov Daniil <adrianwalker3004@gmail.com>
2025-01-10 23:56:08 +0300

    Base buttons

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from application import Application
+import raylib as rl
+from settings import *
+from scenes.scene_manager import handle_key_presses, state
+rl.InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "PAC-MAN Game".encode())  # Инициализация окна
+rl.SetTargetFPS(60)  # Установка целевого FPS
 
-
-def main():
-    app = Application()
-    app.run()
-
-
-if __name__ == '__main__':
-    main()
+# Основной цикл игры
+while not rl.WindowShouldClose():
+    rl.BeginDrawing()
+    handle_key_presses()
+    state.current_scene.draw()
+    rl.EndDrawing()
+rl.CloseWindow()
--- settings.py
+++ settings.py
@@ -1,19 +1,16 @@
-from raylib import colors
-
-
-class Settings:
-    WIDTH = 1000
-    HEIGHT = 800
-    FPS = 60
-    BACKGROUND_COLOR = colors.BLACK
-    scene_changed = True
-    scene_index = 1
-
-    @staticmethod
-    def set_scene(index):
-        Settings.scene_changed = True
-        Settings.scene_index = index
-
-    @staticmethod
-    def set_game_scene():
-        Settings.set_scene(1)
+from raylib import LoadFont, GetFontDefault
+SCREEN_WIDTH = 800  # Ширина окна
+SCREEN_HEIGHT = 600  # Высота окна
+YELLOW_BACKGROUND = (255, 255, 0, 255)  # Желтый фон
+BLACK_BACKGROUND = (0, 0, 0, 255)  # Черный фон
+WHITE_TEXT = (255, 255, 255, 255)  # Белый текст
+RED_TEXT = (255, 0, 0, 255)  # Красный текст
+FONT_SIZE = 40  # Размер шрифта
+BUTTON_WIDTH = 200  # Ширина кнопки
+BUTTON_HEIGHT = 50  # Высота кнопки
+# Загрузка шрифта
+try:
+    font = LoadFont("ofont.ru_Zeitmax.ttf".encode())  # Путь к шрифту
+except Exception as e:
+    print(f"Ошибка загрузки шрифта: {e}")
+    font = GetFontDefault()  # Используем стандартный шрифт, если пользовательский не загрузился
