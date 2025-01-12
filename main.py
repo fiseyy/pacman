b124893c294b0e3588879ab2b334e11ac11e6d11
Diakov Daniil <adrianwalker3004@gmail.com>
2025-01-12 14:53:37 +0300

    final switch to pyray and deleted ".encode()" to switch from raylib to pyray

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import pyray as pr
 from settings import *
-from scenes.scene_manager import handle_key_presses, state
-pr.init_window(SCREEN_WIDTH, SCREEN_HEIGHT, "PAC-MAN Game".encode())  # Инициализация окна
+# from scenes.scene_manager import handle_key_presses, state
+pr.init_window(SCREEN_WIDTH, SCREEN_HEIGHT, "PAC-MAN Game")  # Инициализация окна
 pr.set_target_fps(60)  # Установка целевого FPS
 
 # Основной цикл игры
-while not pr.WindowShouldClose():
+while not pr.window_should_close():
     pr.begin_drawing()
     handle_key_presses()
     state.current_scene.draw()
