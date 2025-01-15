bb2a10a5c623d47be39b0007dfd25c85fe0a30b2
Анастасия <naumkana22@gmail.com>
2025-01-15 22:11:49 +0300

    Added new class Energizer

--- objects/images/seeds.py
+++ objects/images/seeds.py
@@ -17,3 +17,18 @@ class Seed(Circle):
     def init(self, x, y, radius = 10, color=None, weight = 10):
         super().init(x, y, radius, pyray.YELLOW)
         self.weight = weight
+
+class Energizer(Circle):
+    """
+    Класс большого зерна
+
+    Args:
+        x: Координата по х
+        y: Координата по y
+        radius: Радиус круга
+        color: Цвет круга
+        weight: Вес зерна, по умолчанию 50
+    """
+    def init(self, x, y, radius = 20, color=None, weight = 50):
+        super().init(x, y, radius, pyray.YELLOW)
+        self.weight = weight
