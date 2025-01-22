feb1152a8f9b13c45b74d385262f672abc8129d8
Diakov Daniil <adrianwalker3004@gmail.com>
2025-01-22 20:37:42 +0300

    Show textures instead of rectangles

--- objects/ghosts/base.py
+++ objects/ghosts/base.py
@@ -1,13 +1,28 @@
 import pyray as pr
 import random
 from logic.field import Field
+
+# Класс для преобразования изображений в текстуры
+class Textures:
+    def __init__(self):
+        self.seed = pr.load_texture_from_image(pr.load_image("images/corn_1.png")) # текстура малого зерна
+        self.energizer = pr.load_texture_from_image(pr.load_image("images/corn_2.png")) # текстура большого зерна
+        self.textures={
+            "seed": self.seed,
+            "energizer": self.energizer
+        }
+    def get_texture(self, texture_name):
+        return self.textures[texture_name]
+    def unload(self):
+        pr.unload_texture(self.seed)
+        pr.unload_texture(self.energizer)
+
 # Класс для отрисовки поля
 class FieldDrawer:
     def __init__(self, field:Field, cell_size):
         self.field = field.load_field()
-        print(self.field)
         self.cell_size = cell_size
-
+        self.textures = Textures()
     def draw(self):
         for y, row in enumerate(self.field):
             for x, cell in enumerate(row):
@@ -17,10 +32,14 @@ class FieldDrawer:
                     pr.draw_rectangle(x * self.cell_size, y * self.cell_size, self.cell_size, self.cell_size, pr.BLACK)  # Пустые ячейки
                 elif cell == '.':
                     # малое зерно
-                    pr.draw_rectangle(x * self.cell_size, y * self.cell_size, self.cell_size, self.cell_size, pr.YELLOW)
+                    texture = self.textures.get_texture("seed")
+                    pr.draw_texture(texture, x * self.cell_size,y * self.cell_size,pr.WHITE)
+                    #pr.draw_rectangle(x * self.cell_size, y * self.cell_size, self.cell_size, self.cell_size, pr.YELLOW)
                 elif cell == 'S':
                     # большое зерно
-                    pr.draw_rectangle(x * self.cell_size, y * self.cell_size, self.cell_size, self.cell_size, pr.YELLOW)
+                    texture = self.textures.get_texture("energizer")
+                    pr.draw_texture(texture, x * self.cell_size,y * self.cell_size,pr.WHITE)
+                    # pr.draw_rectangle(x * self.cell_size, y * self.cell_size, self.cell_size, self.cell_size, pr.YELLOW)
                 elif cell == 'T':
                     pr.draw_rectangle(x * self.cell_size, y * self.cell_size, self.cell_size, self.cell_size, pr.YELLOW)  # Порталы
 
@@ -135,5 +154,6 @@ class GhostWallDirectionChooser:
                 self.ghost.set_direction(self.dminus(direction))
 
 # Пример запуска
+# if __name__ == "__main__":
 #     field = Field("C:\\git\\pacman-rl-2-group\\logic\\field.txt")
-#     field_drawer = FieldDrawer(field,15)
+#     field_drawer = FieldDrawer(field,20)
