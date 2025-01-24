77d24e6a8cab2e0fee652d74f6e5170b2499843e
Diakov Daniil <adrianwalker3004@gmail.com>
2025-01-24 14:23:37 +0300

    changed pos of ghost

--- scenes/game.py
+++ scenes/game.py
@@ -21,7 +21,7 @@ class GameScene(SceneBase):
         self.textures = Textures()
         self.field = Field("objects/maps/field.txt")
         self.field_drawer = FieldDrawer(self.field,cell_size)
-        self.inky_ghost = InkyGhost(x=1, y=14,cell_size=cell_size, field=self.field,textures=self.textures)
+        self.inky_ghost = InkyGhost(x=1, y=1,cell_size=cell_size, field=self.field,textures=self.textures)
         self.pacman = Pacman(12,1,cell_size, self.textures.get_texture("pacman"))
         self.cherry=Cherry(1,2,5,cell_size)
         super().__init__()
