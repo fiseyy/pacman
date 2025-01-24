2adbf4af9203ca7e3affdc4f898102a82682bbd4
Diakov Daniil <adrianwalker3004@gmail.com>
2025-01-24 14:22:11 +0300

    Realization of portals

--- objects/ghosts/base.py
+++ objects/ghosts/base.py
@@ -3,7 +3,7 @@ import random
 from objects.field import Field
 # Привидения
 class Ghost:
-    def __init__(self,x=0,y=0,cell_size=10, direction=(0,1), field:Field=None, textures=None):
+    def __init__(self,x=0,y=0,cell_size=10, direction=(-1,0), field:Field=None, textures=None):
         self.pos_x = x * cell_size # координата по горизонтали (x)
         self.pos_y = y * cell_size # координата по вертикали (y)
         self.__cell_size = cell_size
@@ -15,12 +15,14 @@ class Ghost:
         self.texture = None # текущая текстура в зависимости от направления
         self.field = field # предпологается, что поле не пустое
         self.changed_direction_on_crossroad = False
+        self.teleported_to_portal = False
     def update(self):
         # движение в зависимости от direction и свободных клеток слева/справа
         # print(self.direction.can_move(), self.direction.get_direction())
         print(self.movement.can_move(), self.movement.get_direction())
         if not self.movement.is_crossroad() and not self.movement.is_portal():
             self.changed_direction_on_crossroad = False
+            self.teleported_to_portal = False
             if self.movement.can_move():
                 self.pos_x += self.movement.get_direction()[0] * self.speed
                 self.pos_y += self.movement.get_direction()[1] * self.speed
@@ -56,7 +58,26 @@ class Ghost:
                         self.movement.pos_cell_x = self.pos_cell_x
                         self.movement.pos_cell_y = self.pos_cell_y
         elif self.movement.is_portal():
-            self.movement.teleport()
+            if not self.teleported_to_portal:
+                portal_positions = self.movement.get_portal_positions()
+                print(portal_positions)
+                portal_positions.remove((self.pos_cell_x, self.pos_cell_y))
+                self.goto(portal_positions[0])
+                self.teleported_to_portal = True
+            else:
+                if self.movement.can_move():
+                    self.pos_x += self.movement.get_direction()[0] * self.speed
+                    self.pos_y += self.movement.get_direction()[1] * self.speed
+                    # if isinstance(self.pos_x/self.__cell_size, int):
+                    if self.pos_x/self.__cell_size % 1 == 0 and self.pos_y/self.__cell_size % 1 == 0:
+                        #DEBUG PRINT
+                        #print(self.pos_x, self.pos_y, self.pos_x/self.__cell_size % 1 == 0 and self.pos_y/self.__cell_size % 1 == 0, self.pos_x/self.__cell_size, self.pos_y/self.__cell_size)
+                        # можно заменять pos_cell_x и pos_cell_y
+                        self.pos_cell_x = int(self.pos_x / self.__cell_size)
+                        self.pos_cell_y = int(self.pos_y / self.__cell_size)
+                        # также поменяем в direction
+                        self.movement.pos_cell_x = self.pos_cell_x
+                        self.movement.pos_cell_y = self.pos_cell_y
     def draw(self):
         #взятие нужной модели
         if self.movement.get_direction() == (1, 0):
@@ -71,7 +92,15 @@ class Ghost:
             self.texture = self.textures["right"]
         # отрисовка на нужной координате
         pr.draw_texture(self.texture, self.pos_x-2, self.pos_y-2, pr.WHITE)
-
+    def goto(self,target_cell_pos: tuple):
+        target_cell_x = target_cell_pos[0]
+        target_cell_y = target_cell_pos[1]
+        self.pos_cell_x = target_cell_x
+        self.pos_cell_y = target_cell_y
+        self.pos_x = self.pos_cell_x * self.__cell_size
+        self.pos_y = self.pos_cell_y * self.__cell_size
+        self.movement.pos_cell_x = self.pos_cell_x
+        self.movement.pos_cell_y = self.pos_cell_y
     
 class GhostMovement:
     def __init__(self, pos_cell_x, pos_cell_y, direction, field):
@@ -96,6 +125,14 @@ class GhostMovement:
         return self.direction
     def set_direction(self, direction):
         self.direction = direction
+    def get_portal_positions(self):
+        portal_positions = []
+        for y in range(len(self.field)):  # Перебираем индексы строк
+            for x in range(len(self.field[y])):  # Перебираем индексы столбцов
+                if self.field.to_array()[x][y] == "T":  # Проверяем, есть ли "T" в текущей позиции
+                    portal_positions.append((x, y))
+        return portal_positions
+  
     def get_possible_directions(self):
         directions = [(1,0), (-1, 0), (0,1), (0,-1)]
         possible_directions = []
@@ -104,16 +141,17 @@ class GhostMovement:
                 possible_directions.append((dx,dy))
         return possible_directions
     def can_move(self, dx = None, dy = None):
-
         if dx is None:
             dx = self.direction[0]
         if dy is None:
             dy = self.direction[1]
-            
         #print(self.field.to_array()[self.pos_cell_x + dx][self.pos_cell_y + dy])
         # Field.to_array() возвращает list с list'ами
-        if self.field.to_array()[self.pos_cell_x + dx][self.pos_cell_y + dy] != "#":
-            return True
+        try:
+            if self.field.to_array()[self.pos_cell_x + dx][self.pos_cell_y + dy] != "#":
+                return True
+        except IndexError:
+            return False
         return False
     
     def is_crossroad(self):
@@ -127,8 +165,6 @@ class GhostMovement:
         return count >= 3  # Перекресток, если 3 или 4 направления
     def is_portal(self):
         return self.field.to_array()[self.pos_cell_x][self.pos_cell_y] == "T"
-    def teleport(self):
-        pass
     def reverse_direction(self, direction):
         return (-direction[0], -direction[1])
     
